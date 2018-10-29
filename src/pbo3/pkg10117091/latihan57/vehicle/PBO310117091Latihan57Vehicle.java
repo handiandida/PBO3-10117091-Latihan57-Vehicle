5739e4739b78ff7c8bf144269c7128b3519b249c
Dida Handian <handiandida@gmail.com>
2018-10-29 20:57:39 +0700

    Vehicle

---
+++ src/pbo3/pkg10117091/latihan57/vehicle/PBO310117091Latihan57Vehicle.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo3.pkg10117091.latihan57.vehicle;
+
+/**
+ *
+ * @author 
+ * NAMA  : Dida Handian
+ * Kelas : PBO3
+ * NIM   : 10117091
+ * Deskripsi Tugas : Membuat Program Info Vehicle
+ *  
+ */
+public class PBO310117091Latihan57Vehicle {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Bicycle bicycle = new Bicycle();
+        Skateboard skateboard = new Skateboard();
+    }
+    
+}
